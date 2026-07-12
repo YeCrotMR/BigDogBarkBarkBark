91743c379b7e7666e3fd878c17bb7f4f52fe47fc
ljy <1234567@qq.com>
2026-07-12 18:34:49 +0800

    0.2.1

---
+++ Source/BigDogBarkBarkBark/BigDogBarkBarkBarkGameModeBase.h
@@ -0,0 +1,13 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BigDogBarkBarkBarkGameModeBase.generated.h"
+
+UCLASS()
+class BIGDOGBARKBARKBARK_API ABigDogBarkBarkBarkGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+};
---
+++ Source/BigDogBarkBarkBarkEditor.Target.cs
@@ -0,0 +1,14 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class BigDogBarkBarkBarkEditorTarget : TargetRules
+{
+	public BigDogBarkBarkBarkEditorTarget(TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Editor;
+		DefaultBuildSettings = BuildSettingsVersion.Latest;
+		ExtraModuleNames.AddRange(new string[] { "BigDogBarkBarkBark" });
+	}
+}
